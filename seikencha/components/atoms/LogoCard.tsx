c7c7b07bf5a9260847a050f0a016082aa3552a82
NAKAJIMA <piyo.0912@gmail.com>
2022-07-08 22:16:23 +0900

    logoのimg修正

--- seikencha/components/atoms/LogoCard.tsx
+++ seikencha/components/atoms/LogoCard.tsx
@@ -1,13 +1,11 @@
 import React from 'react'
-import Image from 'next/image'
-
 
 type LogoCardProps = {
   height?: number
 }
 export const LogoCard: React.FC<LogoCardProps> = (props) => {
   return (
-    <Image
+    <img
       src='/image/logo.png'
       alt='制限鍵盤茶屋'
       height={props.height ? props.height : 40}
