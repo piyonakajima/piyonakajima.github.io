2d1c3aad0944ba67cf07507eee71f90da78ba6d9
NAKAJIMA <piyo.0912@gmail.com>
2022-07-09 18:31:46 +0900

    クリックしたらログを出す

--- seikencha/components/organisms/AlbumCard.tsx
+++ seikencha/components/organisms/AlbumCard.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, CardActions, CardContent, CardMedia, Typography } from '@mui/material'
+import { Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Typography } from '@mui/material'
 import React from 'react'
 import { ShoppingLinkIcon } from '../molecules/ShoppingLinkIcon'
 
@@ -16,53 +16,56 @@ export const AlbumCard: React.FC<AlbumCardProps> = (props) => {
 
   return (
     <Card sx={{ maxWidth: props.maxWidth }}>
-      <CardMedia
-        component="img"
-        height={props.isCD ? props.maxWidth : props.maxWidth * 1.29}
-        image={props.image}
-        alt={props.alt}
-      />
-      <CardContent>
-        <Typography
-          gutterBottom
-          sx={{
-            fontWeightBold: 'FontWeight',
-            fontSize: props.name.length > 10 ? 13 : 18,
-            fontFamily: 'serif',
-            color: '#542218',
-            whiteSpace: 'nowrap'
-          }}>
-          {props.name}
-        </Typography>
-        <Typography
-          sx={{
-            fontWeightBold: 'FontWeight',
-            fontSize: 14,
-            fontFamily: 'serif',
-            color: '#542218',
-            whiteSpace: 'nowrap'
-          }}>
-          {props.description}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <ShoppingLinkIcon url={props.shoppingUrl} />
-        <Button
-          variant="outlined"
-          sx={{ borderColor: '#542218' }}
-          href={props.specialUrl}>
+      <CardActionArea
+        onClick={() => { console.log('first') }}>
+        <CardMedia
+          component="img"
+          height={props.isCD ? props.maxWidth : props.maxWidth * 1.29}
+          image={props.image}
+          alt={props.alt}
+        />
+        <CardContent>
           <Typography
+            gutterBottom
             sx={{
               fontWeightBold: 'FontWeight',
-              fontSize: 12,
+              fontSize: props.name.length > 10 ? 13 : 18,
               fontFamily: 'serif',
               color: '#542218',
               whiteSpace: 'nowrap'
             }}>
-            特設サイト
+            {props.name}
           </Typography>
-        </Button>
-      </CardActions>
+          <Typography
+            sx={{
+              fontWeightBold: 'FontWeight',
+              fontSize: 14,
+              fontFamily: 'serif',
+              color: '#542218',
+              whiteSpace: 'nowrap'
+            }}>
+            {props.description}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <ShoppingLinkIcon url={props.shoppingUrl} />
+          <Button
+            variant="outlined"
+            sx={{ borderColor: '#542218' }}
+            href={props.specialUrl}>
+            <Typography
+              sx={{
+                fontWeightBold: 'FontWeight',
+                fontSize: 12,
+                fontFamily: 'serif',
+                color: '#542218',
+                whiteSpace: 'nowrap'
+              }}>
+              特設サイト
+            </Typography>
+          </Button>
+        </CardActions>
+      </CardActionArea>
     </Card >
   )
 }
